13f5994b371ffabc88b06a9806b4b696f36969a2
j-rilling <jorgea.rilling@gmail.com>
2019-10-31 13:44:43 -0700

    Small changes for performance on v1.2
    Small changes were done in order to improve performance on v1.2 of the
    simulator. It seems that this will not be perfect without changing the
    waypoint updater, but since it works well on v1.3 we decided to not change
    it further.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -47,7 +47,7 @@ class TLDetector(object):
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
 
-        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=10)
+        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
         self.light_classifier = TLClassifier(self.config['is_site'])
